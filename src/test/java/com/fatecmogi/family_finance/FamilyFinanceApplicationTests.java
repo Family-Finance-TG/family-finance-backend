9bedeacfa953d1f5ef43189735b91f4e1846260a
Breno Andrade <breno.andrade1805@gmail.com>
2024-10-30 22:28:10 -0300

    initial commit

---
+++ src/test/java/com/fatecmogi/family_finance/FamilyFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatecmogi.family_finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
