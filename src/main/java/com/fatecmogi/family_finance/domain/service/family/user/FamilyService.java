dfbd3e0735cb8f268b0b84ad1c18917818817b95
Breno Andrade <breno.andrade1805@gmail.com>
2024-11-29 15:47:03 -0300

    feat: add admin role to the user when creating a new family

--- src/main/java/com/fatecmogi/family_finance/domain/service/family/user/FamilyService.java
+++ src/main/java/com/fatecmogi/family_finance/domain/service/family/user/FamilyService.java
@@ -6,8 +6,10 @@ import com.fatecmogi.family_finance.domain.mapper.family.FamilyMapper;
 import com.fatecmogi.family_finance.domain.mapper.user.UserMapper;
 import com.fatecmogi.family_finance.domain.util.AuthUserRecover;
 import com.fatecmogi.family_finance.infrastructure.entity.Family;
+import com.fatecmogi.family_finance.infrastructure.entity.Role;
 import com.fatecmogi.family_finance.infrastructure.entity.User;
 import com.fatecmogi.family_finance.infrastructure.repository.FamilyRepository;
+import com.fatecmogi.family_finance.infrastructure.repository.RoleRepository;
 import com.fatecmogi.family_finance.infrastructure.repository.UserRepository;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
@@ -18,12 +20,14 @@ public class FamilyService {
     private final UserRepository userRepository;
     private final FamilyMapper familyMapper;
     private final AuthUserRecover authUserRecover;
+    private final RoleRepository roleRepository;
 
-    public FamilyService(FamilyRepository familyRepository, UserRepository userRepository, FamilyMapper familyMapper, UserMapper userMapper, AuthUserRecover authUserRecover) {
+    public FamilyService(FamilyRepository familyRepository, UserRepository userRepository, FamilyMapper familyMapper, UserMapper userMapper, AuthUserRecover authUserRecover, RoleRepository roleRepository) {
         this.familyRepository = familyRepository;
         this.userRepository = userRepository;
         this.familyMapper = familyMapper;
         this.authUserRecover = authUserRecover;
+        this.roleRepository = roleRepository;
     }
 
     private void savePre(FamilyDTO dto, Family entity) {
@@ -44,6 +48,7 @@ public class FamilyService {
 
     private void savePos(FamilyDTO dto, Family entity, User creator) {
         creator.setFamily(entity);
+        creator.getRoles().add(roleRepository.findByValue(Role.Values.ADMIN.getValue()));
         userRepository.save(creator);
     }
 
