7f305cc2031e38f58461db46ae8bfc2258f4c2e9
Breno Andrade <breno.andrade1805@gmail.com>
2024-11-29 16:21:53 -0300

    feat: create route to add new member

--- src/main/java/com/fatecmogi/family_finance/application/controller/family/FamilyController.java
+++ src/main/java/com/fatecmogi/family_finance/application/controller/family/FamilyController.java
@@ -2,7 +2,7 @@ package com.fatecmogi.family_finance.application.controller.family;
 
 import com.fatecmogi.family_finance.application.dto.family.FamilyDTO;
 import com.fatecmogi.family_finance.application.util.AppResponseData;
-import com.fatecmogi.family_finance.domain.service.family.user.FamilyService;
+import com.fatecmogi.family_finance.domain.service.family.FamilyService;
 import com.fatecmogi.family_finance.domain.service.user.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
@@ -29,8 +29,18 @@ public class FamilyController {
         );
     }
 
+    @PatchMapping("/{familyId}/users/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public AppResponseData addMember(@PathVariable("familyId") long familyId, @PathVariable("userId") long userId, JwtAuthenticationToken token) {
+        return new AppResponseData(
+                HttpStatus.OK.value(),
+                HttpStatus.OK.name(),
+                familyService.addMember(familyId, userId, token)
+        );
+    }
+
     @GetMapping("/{id}")
-    public AppResponseData readById(@PathVariable("id") Long id) {
+    public AppResponseData readById(@PathVariable("userId") Long id) {
         return new AppResponseData(
                 HttpStatus.OK.value(),
                 HttpStatus.OK.name(),
