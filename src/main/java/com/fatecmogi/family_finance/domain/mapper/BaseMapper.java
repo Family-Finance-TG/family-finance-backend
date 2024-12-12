1bf5e31c8fafaf100ad6b0ea07856bae7e954044
Breno Andrade <breno.andrade1805@gmail.com>
2024-12-12 19:52:30 -0300

    feat: add mapstruct on project and implement mappers

--- src/main/java/com/fatecmogi/family_finance/domain/mapper/BaseMapper.java
+++ src/main/java/com/fatecmogi/family_finance/domain/mapper/BaseMapper.java
@@ -1,9 +1,10 @@
 package com.fatecmogi.family_finance.domain.mapper;
 
 import com.fatecmogi.family_finance.application.dto.IDTO;
+import org.mapstruct.MappingTarget;
 
-public interface IMapper<E, D extends IDTO> {
+public interface BaseMapper <E, D extends IDTO> {
     D toDTO(E entity);
     E toEntity(D dto);
-    void updateEntity(D dto, E entity);
+    void updateEntity(D dto, @MappingTarget E entity);
 }
